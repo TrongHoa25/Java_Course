60fd7302bfe254c836240cd262370e2f22265080
TrongHoa25 <nhokteti2509@gmail.com>
2018-04-20 19:44:44 +0700

    thuchanh1

--- Java_Course_TH01/src/dnhthanh/hueic/edu/vn/MainClass.java
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/MainClass.java
@@ -15,29 +15,29 @@ public class MainClass {
 		s = br.readLine();
 		scon = br.readLine();
 		//Gọi hàm đảo xâu
-		/*System.out.println(DaoXau.thuchien(s));
+		System.out.println(DaoXau.thuchien(s));
 		//Gọi hàm đảo từ
-		System.out.println(DaoTu.thuchien(s));*/
+		System.out.println(DaoTu.thuchien(s));
 		ViTri.xacdinhvt(s,scon);
 
 		
-//		//Nhập vào mảng theo định dạng các phần tử cách nhau bằng kt trắng 1 2 3
-//		String input = br.readLine();
-//		//Tách các phần tử bằng cách chia nhỏ xâu theo dấu hiệu là kt trắng
-//		String[] input_element = input.split(" ");//chứa các phần tử ở dạng xâu kt
-//		float mang[] = new float[input_element.length];
-//		for(int i=0; i<mang.length; i++)
-//			mang[i] = Float.parseFloat(input_element[i]);//chứa các phần tử dạng số
-//		System.out.println("Tổng mảng là: "+TinhTong.thuchien(mang));
+		//Nhập vào mảng theo định dạng các phần tử cách nhau bằng kt trắng 1 2 3
+		String input = br.readLine();
+		//Tách các phần tử bằng cách chia nhỏ xâu theo dấu hiệu là kt trắng
+		String[] input_element = input.split(" ");//chứa các phần tử ở dạng xâu kt
+		float mang[] = new float[input_element.length];
+		for(int i=0; i<mang.length; i++)
+			mang[i] = Float.parseFloat(input_element[i]);//chứa các phần tử dạng số
+		System.out.println("Tổng mảng là: "+TinhTong.thuchien(mang));
 		
-		//Nhập hệ số a, b, c của phương trình bậc 2 theo định dạng a b c
-//		float a, b, c;
-//		String input2 = br.readLine();
-//		String[] input_element2 = input2.split(" ");
-//		a = Float.parseFloat(input_element2[0]);
-//		b = Float.parseFloat(input_element2[1]);
-//		c = Float.parseFloat(input_element2[2]);
-//		GPT.thuchien(a, b, c);
+//		Nhập hệ số a, b, c của phương trình bậc 2 theo định dạng a b c
+		float a, b, c;
+		String input2 = br.readLine();
+		String[] input_element2 = input2.split(" ");
+		a = Float.parseFloat(input_element2[0]);
+		b = Float.parseFloat(input_element2[1]);
+		c = Float.parseFloat(input_element2[2]);
+		GPT.thuchien(a, b, c);
 	}
 
 }
